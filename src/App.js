e6fe3f11974fa4dc54470a75f8783aac42096f29
Tomixer <tomix.klokar@gmail.com>
2023-10-08 14:02:26 +0200

    filtrovanie filmov

--- src/App.js
+++ src/App.js
@@ -1,7 +1,29 @@
-
+import {useState} from "react"
+import allMovies from "./data"
+import categories from "./categories"
 const App=()=>{
+const [typeOfMovie,settypeOfMovie]=useState("komedie")
+
+const vyslednefilmi=allMovies.filter((onemovie)=>{
+  return onemovie["category"]===typeOfMovie
+})
+  console.log(vyslednefilmi)
   return <div>
-    <h1>test</h1>
+    <div>
+      {
+        vyslednefilmi.map((onemovie)=>{
+          const {id,image,title,age,tags,description}=onemovie
+
+          return <div>
+            <img src={image} alt="" />
+            <h2>{title}</h2>
+            <p>{age}</p>
+            <p>{tags}</p>
+            <p>{description}</p>
+          </div>
+        })
+      }
+    </div>
   </div>
 }
 export default App
